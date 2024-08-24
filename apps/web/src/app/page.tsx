8b3acc4e48d6d3140522ab1a352493211324329e
Romes <thonyromes@gmail.com>
2024-08-24 15:05:34 +0100

    feat: complete workspaces setup

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
+
 import styles from "./page.module.css";
+
+import { Button } from "@rotate/ui";
 
 export default function Home() {
   return (
@@ -9,6 +12,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
+        <Button>Test Button</Button>
         <div>
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,10 @@
+import {
+  Button as ChakraButton,
+  ButtonProps as ChakraButtonProps,
+} from "@chakra-ui/react";
+
+export type ButtonProps = ChakraButtonProps;
+
+export const Button = ({ children, ...props }: ButtonProps) => {
+  return <ChakraButton {...props}>{children}</ChakraButton>;
+};
