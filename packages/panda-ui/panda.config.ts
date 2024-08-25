4044d0a2bca7b211ee7c00b944446618191083ed
Romes <thonyromes@gmail.com>
2024-08-25 13:26:10 +0100

    feat: attempt to fix preset not applying

--- packages/panda-ui/panda.config.ts
+++ packages/panda-ui/panda.config.ts
@@ -6,7 +6,11 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{ts,tsx,js,jsx}"],
+  include: [
+    "./src/**/*.{ts,tsx,js,jsx}",
+    "./node_modules/@rotate/panda-preset/src/**/*.tsx",
+    "./node_modules/@rotate/panda-preset/dist/panda.buildinfo.json",
+  ],
 
   // Files to exclude
   exclude: [],
@@ -18,4 +22,5 @@ export default defineConfig({
   outdir: "styled-system",
   jsxFramework: "react",
   outExtension: "js",
+  importMap: "@rotate/panda-preset",
 });
